1bc95979707234aca73907706adf0e997bff8db8
pedrojaraujo <peroa990@gmail.com>
2025-03-30 17:28:30 -0300

    adiciona arquivo LICENSE e atualiza README.md para referência à licença; modifica UserController para validar entrada e retornar respostas apropriadas

--- src/main/java/org/pedrojaraujo/controllers/UserController.java
+++ src/main/java/org/pedrojaraujo/controllers/UserController.java
@@ -4,6 +4,7 @@ import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import org.pedrojaraujo.TaskUser;
 import org.pedrojaraujo.repositories.UserRepository;
 import java.util.List;
@@ -24,7 +25,16 @@ public class UserController {
 
     @POST
     @Transactional
-    public void createUser(TaskUser user) {
+    public Response createUser(TaskUser user) {
+
+        if (user == null || user.getName() == null || user.getName().trim().isEmpty()
+                || user.getEmail() == null || user.getEmail().trim().isEmpty()) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("Erro: Solicitamos apenas o nome de usuário e o e-mail, ambos obrigatórios.")
+                    .build();
+        }
+
         userRepository.persist(user);
+        return Response.status(Response.Status.CREATED).entity(user).build();
     }
 }
